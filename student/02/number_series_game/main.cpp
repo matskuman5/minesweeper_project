1bd276c9642437a76803ab25aef0c8da2c066b25
Matias Selin <matias.selin@tuni.fi>
2021-09-04 12:32:37 +0300

    Lisätty myös 7:llä jaollisten lukujen käsittely

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -10,8 +10,12 @@ int main()
     cin >> numerojen_maara;
 
     for (int i = 1; i <= numerojen_maara; ++i) {
-        if (i % 3 == 0) {
+        if (i % 3 == 0 && i % 7 == 0) {
+            cout << "zip boing" << endl;
+        } else if (i % 3 == 0) {
             cout << "zip" << endl;
+        } else if (i % 7 == 0) {
+            cout << "boing" << endl;
         } else {
             cout << i << endl;
         }

e5fccca50ecaf7903d3646332fa479ba484043e1
Matias Selin <matias.selin@tuni.fi>
2021-09-12 14:30:50 +0300

    initial implementation of bank_account

---
+++ student/03/bank_account/account.cpp
@@ -0,0 +1,77 @@
+#include "account.hh"
+#include <iostream>
+#include <string>
+
+Account::Account(const std::string& owner, bool has_credit):
+    owner_(owner), has_credit_(has_credit) {
+    generate_iban();
+}
+
+// Setting initial value for the static attribute running_number_
+int Account::running_number_ = 0;
+
+void Account::generate_iban()
+{
+    ++running_number_;
+    std::string suffix = "";
+    if(running_number_ < 10)
+    {
+        suffix.append("0");
+    }
+    else if(running_number_ > 99)
+    {
+        std::cout << "Too many accounts" << std::endl;
+    }
+    suffix.append(std::to_string(running_number_));
+
+    iban_ = "FI00 1234 ";
+    iban_.append(suffix);
+}
+
+void Account::print() const {
+
+    std::cout << owner_ << " : " << iban_ << " : " << balance_ << std::endl;
+
+}
+
+void Account::set_credit_limit(int i) {
+
+    if (has_credit_) {
+        credit_limit_ = i;
+    } else {
+        std::cout << "Cannot set credit limit: the account has no credit card" << std::endl;
+    }
+
+}
+
+void Account::save_money(int i) {
+
+    balance_ += i;
+
+}
+
+bool Account::take_money(int i){
+
+    if (balance_ - i < 0 && !has_credit_) {
+        std::cout << "Cannot take money: balance underflow" << std::endl;
+        return false;
+    } else if (balance_ - i < -credit_limit_ && has_credit_){
+        std::cout << "Cannot take money: credit limit overflow" << std::endl;
+        return false;
+    } else {
+        balance_ -= i;
+        std::cout << i << " euros taken: new balance of " << iban_ << " is " << balance_ << " euros" << std::endl;
+        return true;
+    }
+
+}
+
+void Account::transfer_to(Account& a, int i) {
+
+    if(take_money(i)) {
+        a.save_money(i);
+    } else {
+        std::cout << "Transfer from " << iban_ << " failed" << std::endl;
+    }
+
+}

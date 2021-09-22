07adf0f58a82230ad3cef9a6d1a54ba7388dd03a
Matias Selin <matias.selin@tuni.fi>
2021-09-22 15:32:51 +0300

    initial commit creating the project and adding some sketches for functions

---
+++ student/04/ristinolla/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const unsigned int INIT_BOARD_SIZE = 3;
+using Board = std::vector< std::vector< char > >;
+
+int main() {
+
+}
+
+void printBoard(const Board b) {
+
+}
+
+bool checkCoordinates() {
+
+}
+
+void placeMarker(Board& b) {
+
+}
+
+void extendBoard(Board& b) {
+
+}
+
+void checkEnd() {
+
+}

bcb6c1aabe8003952428306ebaea903500e2e647
Matias Selin <matias.selin@tuni.fi>
2021-10-17 19:52:02 +0300

    implement file reading and the data structure

--- student/07/education_center2/main.cpp
+++ student/07/education_center2/main.cpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <map>
 
 using namespace std;
 
@@ -58,7 +59,66 @@ int main()
 
     if(reader) {
 
-        cout << "success";
+        //basic file structure:
+        //map<struct>
+        //top-level keys are strings (theme names) and values are vectors containing structs (courses)
+
+        struct Course {
+            string name;
+            string location;
+            int enrollments;
+        };
+
+        map<string, vector<Course>> themes;
+
+        string line;
+
+        while(getline(reader, line)) {
+
+            vector<string> parts = split_ignoring_quoted_delim(line, ';');
+            Course c;
+            string theme = parts.at(0);
+            c.name = parts.at(1);
+            c.location = parts.at(2);
+            if (parts.at(3) == "full") {
+                c.enrollments = -1;
+            } else {
+                c.enrollments = stoi(parts.at(3));
+            }
+
+
+
+            bool exists = false;
+            for (auto thing : themes) {
+                if (thing.first == theme) {
+                    exists = true;
+                    break;
+                }
+            }
+
+            if (exists) {
+                //if the theme already exists, just push back the course into its vector
+                themes.at(theme).push_back(c);
+            } else {
+                //otherwise create the associated vector and insert it into the map
+                vector<Course> courses;
+                courses.push_back(c);
+                themes.insert({theme, courses});
+            }
+
+        }
+
+        reader.close();
+
+//        for (auto thing : themes) {
+//            cout << thing.first << ": " << endl;
+//            for (auto thing2 : thing.second) {
+//                cout << thing2.name << ", " << thing2.location << ", " << thing2.enrollments << endl;
+//            }
+//            cout << endl << endl;
+//        }
+
+        cout << "Success!";
         return EXIT_SUCCESS;
 
     } else {

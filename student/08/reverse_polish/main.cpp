64c8968102417a5c3891eeba2d8b972b36af4428
Matias Selin <matias.selin@tuni.fi>
2021-10-31 23:00:57 +0200

    use cin instead of getline

--- student/08/reverse_polish/main.cpp
+++ student/08/reverse_polish/main.cpp
@@ -7,28 +7,53 @@ int main()
     std::cout << "Input an expression in reverse Polish notation (end with #):" << std::endl;
     std::cout << "EXPR>";
 
-    std::string input;
-    getline(std::cin, input);
+    char char_array[80];
 
-    //remove spaces from input
-    for (unsigned long i = 0; i < input.length(); ++i) {
-//        std::cout << "index: " << i << ", input.at(i): " << input.at(i) << std::endl;
-        if (input.at(i) == ' ') {
-            input.erase(i, 1);
-            --i;
+    char to_check;
+
+    int size = 0;
+
+    for (int i = 0; i < 80; ++i) {
+        std::cin >> to_check;
+        if (i == 0) {
+            if (!isdigit(to_check)) {
+                std::cout << "Error: Expression must start with a number" << std::endl;
+                return EXIT_FAILURE;
+            }
         }
+
+        if (to_check == '#') {
+            break;
+        }
+
+        char_array[i] = to_check;
+        size = i;
     }
 
-    //std::cout << "your input without spaces: " << input << std::endl;
+    std::string input = "";
+    for (int i = 0; i <= size; ++i) {
+        input = input + char_array[i];
+    }
+
+//    //remove spaces from input
+//    for (unsigned long i = 0; i < input.length(); ++i) {
+//        std::cout << "index: " << i << ", input.at(i): " << input.at(i) << std::endl;
+//        if (input.at(i) == ' ') {
+//            input.erase(i, 1);
+//            --i;
+//        }
+//    }
+
+//    //std::cout << "your input without spaces: " << input << std::endl;
 
     int array[input.length()];
 
     int* pointer = array - 1;
 
-    if (!isdigit(input.at(0))) {
-        std::cout << "Error: Expression must start with a number" << std::endl;
-        return EXIT_FAILURE;
-    }
+//    if (!isdigit(input.at(0))) {
+//        std::cout << "Error: Expression must start with a number" << std::endl;
+//        return EXIT_FAILURE;
+//    }
 
     for (unsigned long i = 0; i < input.length(); ++i) {
 

ba25516fbcae42c21b09f855f4bdddd54f23b608
Matias Selin <matias.selin@tuni.fi>
2021-11-21 21:51:13 +0200

    add sempty shells for functions to remove errors

--- student/11/directories/directoryhierarchy.cpp
+++ student/11/directories/directoryhierarchy.cpp
@@ -1,1 +1,61 @@
 #include "directoryhierarchy.hh"
+
+DirectoryHierarchy::DirectoryHierarchy()
+{
+
+}
+
+DirectoryHierarchy::~DirectoryHierarchy()
+{
+
+}
+
+void DirectoryHierarchy::addNewDirectory(const std::string &id, const std::string &timestamp, int size, std::ostream &output)
+{
+
+}
+
+void DirectoryHierarchy::addRelation(const std::string &directory, const std::string &parent, std::ostream &output)
+{
+
+}
+
+void DirectoryHierarchy::commandPrintWorkingDirectory(std::ostream &output) const
+{
+
+}
+
+void DirectoryHierarchy::commandList(std::ostream &output) const
+{
+
+}
+
+void DirectoryHierarchy::commandDiskUsage(std::ostream &output) const
+{
+
+}
+
+void DirectoryHierarchy::commandChangeDirectory(const std::string &id, std::ostream &output)
+{
+
+}
+
+void DirectoryHierarchy::commandFind(const std::string &id, const int n, std::ostream &output) const
+{
+
+}
+
+void DirectoryHierarchy::commandAsOldAs(const std::string &id, std::ostream &output) const
+{
+
+}
+
+void DirectoryHierarchy::commandGreatest(const std::string &id, std::ostream &output) const
+{
+
+}
+
+void DirectoryHierarchy::commandSmallest(const std::string &id, std::ostream &output) const
+{
+
+}
